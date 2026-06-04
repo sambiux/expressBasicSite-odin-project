c45656753c4f55e4feccf53bf89171922de799ca
sambiux <sddaza.9@gmail.com>
2026-06-04 23:24:45 +0000

    Simulacion base de datos

---
+++ bd.js
@@ -0,0 +1,229 @@
+const BD = [
+  {
+    "_id": "6a21f6f040120cab02e40aa2",
+    "index": 0,
+    "guid": "40b37d55-9bf0-4525-8c58-6d6af9b41bfe",
+    "isActive": false,
+    "balance": "$3,395.45",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "brown",
+    "name": "Black Hull",
+    "gender": "male",
+    "company": "NETPLODE",
+    "email": "blackhull@netplode.com",
+    "phone": "+1 (821) 557-3034",
+    "address": "690 Revere Place, Kimmell, Georgia, 4673",
+    "about": "Voluptate ea amet ullamco exercitation consectetur amet fugiat excepteur incididunt veniam aute. Amet aute culpa fugiat non fugiat exercitation labore occaecat velit ut minim. Aliquip magna eiusmod sint id irure incididunt exercitation ullamco incididunt magna sint. Eiusmod dolor aute ad excepteur amet commodo in sit est eu. Aute exercitation occaecat ex elit tempor.\r\n",
+    "registered": "2021-12-14T09:48:43 +05:00",
+    "latitude": -8.445835,
+    "longitude": -176.593415,
+    "tags": [
+      "eiusmod",
+      "cillum",
+      "voluptate",
+      "qui",
+      "in",
+      "pariatur",
+      "ex"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Allison Atkinson"
+      },
+      {
+        "id": 1,
+        "name": "Leslie Mcconnell"
+      },
+      {
+        "id": 2,
+        "name": "Kellie Farrell"
+      }
+    ],
+    "greeting": "Hello, Black Hull! You have 4 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "6a21f6f0f7a46dc50aad8bd3",
+    "index": 1,
+    "guid": "60fcd041-43b7-43ee-b404-d7524c83902a",
+    "isActive": false,
+    "balance": "$2,314.39",
+    "picture": "http://placehold.it/32x32",
+    "age": 37,
+    "eyeColor": "brown",
+    "name": "Mavis Anthony",
+    "gender": "female",
+    "company": "CYTREX",
+    "email": "mavisanthony@cytrex.com",
+    "phone": "+1 (896) 566-3084",
+    "address": "355 Lynch Street, Ladera, Virginia, 4344",
+    "about": "Irure aliqua aute do veniam laboris ad incididunt aliquip ullamco. In ad incididunt est est et consequat nisi. Non dolor voluptate consectetur id consequat ut. Culpa sit esse sit veniam voluptate est esse. Irure commodo nisi adipisicing quis duis.\r\n",
+    "registered": "2017-11-12T09:23:26 +05:00",
+    "latitude": -62.17767,
+    "longitude": 30.972164,
+    "tags": [
+      "mollit",
+      "occaecat",
+      "quis",
+      "eiusmod",
+      "duis",
+      "amet",
+      "amet"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Allison Burgess"
+      },
+      {
+        "id": 1,
+        "name": "Martin Curry"
+      },
+      {
+        "id": 2,
+        "name": "Miles Chavez"
+      }
+    ],
+    "greeting": "Hello, Mavis Anthony! You have 1 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "6a21f6f075c3b65adfaf14e9",
+    "index": 2,
+    "guid": "6005dca9-206c-49ce-a9a3-5f1730575552",
+    "isActive": true,
+    "balance": "$3,824.75",
+    "picture": "http://placehold.it/32x32",
+    "age": 40,
+    "eyeColor": "blue",
+    "name": "Daisy Spence",
+    "gender": "female",
+    "company": "INSURESYS",
+    "email": "daisyspence@insuresys.com",
+    "phone": "+1 (879) 468-3844",
+    "address": "905 Celeste Court, Vicksburg, Indiana, 379",
+    "about": "Fugiat nulla ullamco velit sit proident fugiat in reprehenderit. Consectetur proident consectetur cupidatat enim Lorem amet nisi voluptate ullamco veniam fugiat. Amet do consectetur non nulla. Dolor sunt fugiat proident cupidatat est tempor ut. Voluptate commodo cupidatat consequat nisi non nulla occaecat est est cillum culpa anim sunt. Cillum enim esse veniam consectetur Lorem tempor excepteur nisi. Sit ad ea adipisicing voluptate enim qui quis quis tempor pariatur duis reprehenderit consequat.\r\n",
+    "registered": "2023-04-05T09:20:10 +05:00",
+    "latitude": 30.28043,
+    "longitude": -146.467532,
+    "tags": [
+      "Lorem",
+      "ipsum",
+      "ad",
+      "irure",
+      "id",
+      "dolor",
+      "ad"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Kathryn Mosley"
+      },
+      {
+        "id": 1,
+        "name": "Margret Gray"
+      },
+      {
+        "id": 2,
+        "name": "Allyson Wolfe"
+      }
+    ],
+    "greeting": "Hello, Daisy Spence! You have 2 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "6a21f6f0bc95227ef6fb15b6",
+    "index": 3,
+    "guid": "2fef6ff6-0410-4539-88ce-7bb5a76694b8",
+    "isActive": false,
+    "balance": "$2,517.98",
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "eyeColor": "green",
+    "name": "Traci Charles",
+    "gender": "female",
+    "company": "ACUSAGE",
+    "email": "tracicharles@acusage.com",
+    "phone": "+1 (918) 553-2087",
+    "address": "997 Ralph Avenue, Dubois, Florida, 6950",
+    "about": "Exercitation dolor sit nulla ad consequat do veniam pariatur. Laborum enim incididunt reprehenderit esse dolor aliquip sunt pariatur Lorem ea. Commodo esse magna qui voluptate anim Lorem ipsum incididunt ea. Ullamco nulla eiusmod officia in ut commodo occaecat velit cillum culpa sunt. Ipsum amet commodo aute ea ut exercitation labore elit.\r\n",
+    "registered": "2015-11-13T11:22:22 +05:00",
+    "latitude": -47.894902,
+    "longitude": 107.974886,
+    "tags": [
+      "dolore",
+      "nulla",
+      "irure",
+      "occaecat",
+      "culpa",
+      "voluptate",
+      "tempor"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Graham Armstrong"
+      },
+      {
+        "id": 1,
+        "name": "Duffy Buckley"
+      },
+      {
+        "id": 2,
+        "name": "Beatrice Mcleod"
+      }
+    ],
+    "greeting": "Hello, Traci Charles! You have 8 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "6a21f6f0ef642bc392a71acb",
+    "index": 4,
+    "guid": "6f7f4117-47b9-4573-a815-e932265ea769",
+    "isActive": false,
+    "balance": "$1,782.19",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "brown",
+    "name": "Deena Riggs",
+    "gender": "female",
+    "company": "OHMNET",
+    "email": "deenariggs@ohmnet.com",
+    "phone": "+1 (984) 465-2356",
+    "address": "650 Knight Court, Cliff, Michigan, 5473",
+    "about": "Est in amet officia nostrud aute consequat. Irure dolore nostrud quis pariatur consectetur deserunt minim occaecat ad aute aliquip deserunt. Labore eu adipisicing incididunt Lorem ut reprehenderit anim quis. Laborum exercitation eiusmod eiusmod ipsum pariatur nostrud consequat adipisicing nisi cillum nisi nostrud do. Qui sunt ea culpa veniam deserunt sit dolor do adipisicing fugiat amet ipsum laborum aliqua. Incididunt Lorem culpa cillum tempor. Elit sunt non ut fugiat nisi Lorem aliquip nisi ipsum reprehenderit ullamco.\r\n",
+    "registered": "2021-12-03T08:25:55 +05:00",
+    "latitude": 5.093233,
+    "longitude": -3.024316,
+    "tags": [
+      "culpa",
+      "fugiat",
+      "cupidatat",
+      "consectetur",
+      "consectetur",
+      "qui",
+      "fugiat"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Terry Delacruz"
+      },
+      {
+        "id": 1,
+        "name": "Lynne Salas"
+      },
+      {
+        "id": 2,
+        "name": "Katie Hunter"
+      }
+    ],
+    "greeting": "Hello, Deena Riggs! You have 4 unread messages.",
+    "favoriteFruit": "strawberry"
+  }
+]
+
+module.exports = BD;
