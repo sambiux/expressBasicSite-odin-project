a306c9d559d8d63aad8b12f254b6b341a0ce5510
sambiux <sddaza.9@gmail.com>
2026-06-06 04:08:27 +0000

    Agregando metodo POST a las rutas

--- routes/player-route.js
+++ routes/player-route.js
@@ -2,21 +2,15 @@
 const express = require('express')
 const playerRouter = express();
 const BD = require('../bd.js')
-
+const playerController = require('../controllers/playerController.js')
+const createPlayerCntrler= require('../controllers/createPlayerController.js')
 
 playerRouter.get('/', (req, res) =>{
     res.send("Home players")
 })
-playerRouter.get('/:guid', (req, res) => {
-    const player = BD.find(player => player.guid == req.params.guid);
-    console.log(player);
-    if(!player){
-        res.send("Player not found");
-        console.log("Player not found");
-    }
-    res.send(player);
-    
-    
-})
+
+playerRouter.post('/', createPlayerCntrler )
+playerRouter.get('/:guid', playerController)
+
 
 module.exports= playerRouter;
